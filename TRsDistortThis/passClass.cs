41e285b7ecfee90a142e3952b942513777516961
toehead2001 <toe_head2001@yahoo.com>
2017-04-21 03:33:51 -0700

    Just use auto Properties

--- TRsDistortThis/passClass.cs
+++ TRsDistortThis/passClass.cs
@@ -2,31 +2,7 @@
 {
     class passClass
     {
-        static int thid;
-        static string stid;
-        public static int ThreadID
-        {
-            get
-            {
-                return thid;
-            }
-            set
-            {
-                thid = value;
-            }
-        }
-
-        public static string StringID
-        {
-            get
-            {
-                return stid;
-            }
-            set
-            {
-                stid = value;
-            }
-        }
+        internal static int ThreadID { get; set; }
+        internal static string StringID { get; set; }
     }
-
 }
